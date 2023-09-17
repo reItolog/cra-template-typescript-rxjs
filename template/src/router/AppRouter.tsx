f543427760e480f443bbbb422a95119f78c617e2
reitolog <serhiirmn@gmail.com>
2023-09-17 20:37:33 +0300

    update react router

--- template/src/router/AppRouter.tsx
+++ template/src/router/AppRouter.tsx
@@ -1,21 +1,23 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-import Loader from '../shared/UI/Loader/Loader';
 import Home from '../pages/Home/Home';
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+]);
+
+
 const AppRouter: React.FC = () => {
-  return (
-    <Router>
-      <Suspense fallback={<Loader />}>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </Suspense>
-    </Router>
-  );
+    return (
+        <RouterProvider router={router}/>
+    );
 };
 
 export default AppRouter;
